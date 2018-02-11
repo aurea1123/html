2c1aca75e0419ad80d5cb5660ed3a22ad6259e17
cole <cole@prog.com>
2018-02-11 23:03:25 +0100

    nueva_rama

--- horarios/javascript/java.js
+++ horarios/javascript/java.js
@@ -1,33 +1,28 @@
-<!DOCTYPE html>
-<html>
-<head>
-<style type="text/css">
-li.menu{
-display: inline-block;
-padding-right: 1%;
-background-color:  #000;
-margin-left: 10%;
+var aux = "<ul class=menu>";
+var x = 10;
+for (var i = 0; i < x; i++) {
+	aux += "<li class=menu><a href=" + i + ".html target=_blanck>Prueva nª"+ i +"</li>" ;
 }
-ul.menu{
-background-color: #000;
+aux += "</ul>";
+document.getElementById("menu").innerHTML = aux;
+var aux = "<ul class=hora>";
+var x = 2;
+var hora = ["Barcelona", "Ripoll"];
+var dias =["Dilluns", "Dimarts", "Dimecres", "Dijous", "Divenderes", "Disapte", "Diumenge"];
+var h = ["15.30 a 20.00", "15.30 a 20.00", "9.00 a 13.30 i de 15.30 a 20.00", "15.30 a 20.00 ", "9.00 a 13.30 i de 15.30 a 20.00", "10 a 14"]; 
+
+for (var i = 0; i < x; i++) {
+	aux += hora[i] + "<br>";
+	if (hora[i] == "Ripoll") {
+	for (var e = 0; e < 6; e++){
+		aux += dias[e] + "  ";
+		aux += h[e] + "<br>";
+	}
+	}
+	if (hora[i] == "Barcelona") {
+		aux += "<iframe src=http://ajuntament.barcelona.cat/biblioteques/sites/default/files/horaris_v120.pdf></iframe>"
+	}
+	aux += "<br>"
 }
-ul.hora{
-font-size: 200%;
-}
-iframe{
-width: 100%;
-height: 100%;
-}
-</style>
-<meta charset="utf-8" />
-<title>Prueva de javascript</title>
-</head>
-<header>
-<div id="menu">Esto no funciona</div>
-</header>
-<body>
-<div id="hora"></div>
-</body>
-<script type="text/javascript" src="java.js">
-//document.getElementById("menu").innerHTML = "hola";
-</script>
+aux += "</ul>";
+document.getElementById("hora").innerHTML = aux;
